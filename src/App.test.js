5175840c42fb0c7dd2647d92b8db794071a98f59
Remzila <remington.manso@gmail.com>
2019-09-05 14:26:51 +0100

    created first test for  header with code

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+// it('renders "Hello World!', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   expect(div.textContent).toBe('Hello World');
+// });
+
+describe("Header",() =>{
+  it('Displays "Hello World', () => {
+    const div =document.createElement('div');
+    ReactDOM.render(<Header />, div);
+    expect(div.textContent).toBe('Hello World');
+  });
 });
